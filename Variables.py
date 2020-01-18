9eacec5bb7dbc18cfda0b2bba8b55078c7e3f4a1
csebisht93 <csebisht@gmail.com>
2020-01-18 19:07:04 +0530

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,58 @@
+lst = [25, 24, 21, 26]
+print(lst)
+print(type(lst))
+print(id(lst))
+
+seq = {25, 24, 21, 26}
+print(seq)
+print(type(seq))
+print(id(seq))
+
+tup = (25, 24, 21, 26)
+print(tup)
+print(type(tup))
+print(id(tup))
+
+srt = "(25, 24, 21, 26)"
+print(srt)
+print(type(srt))
+print(id(srt))
+
+cha = 'P'
+print(cha)
+print(type(cha))
+print(id(cha))
+
+age = 1
+print(age)
+print(id(age))
+print(type(age))
+
+Pre = 1
+print(Pre)
+print(id(Pre))
+print(type(Pre))
+
+fla = 1.12
+print(fla)
+print(id(fla))
+print(type(fla))
+
+fsa = 1.12
+print(fsa)
+print(id(fsa))
+print(type(fsa))
+
+com = 5 + 6j
+print(com)
+print(id(com))
+print(type(com))
+
+com1 = 5 + 6j
+print(com1)
+print(id(com1))
+print(type(com1))
+
+Ran = range(10)
+print(Ran)
+print(list(Ran))
