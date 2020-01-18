afae18d649fa5513eb808833538d2a4e9eab956a
csebisht93 <csebisht@gmail.com>
2020-01-18 19:44:23 +0530

    Conversion File

---
+++ Conversion.py
@@ -0,0 +1,18 @@
+# Convert From float to int
+a = 5.6
+b = int(a)
+
+print("a as float", a)
+print("after conversion of a as int b is", b)
+
+# int to float
+
+k = float(b)
+print("b as a int convert into float", k)
+
+c = complex(b, k)
+print("using b and k we creating the complex variable", c)
+
+print("Boolean value by comparing the value b > k where b=5 and k=5.0 and result is ", b > k)
+print("Boolean value by comparing the value b < k where b=5 and k=5.0 and result is ", b < k)
+print("Boolean value by comparing the value b == k where b=5 and k=5.0 and result is ", b == k)
